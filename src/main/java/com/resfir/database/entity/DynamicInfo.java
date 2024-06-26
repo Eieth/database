edd09943287dff29444787cdca9dcdeb4574bc8b
LeKetian <2106010228@hhu.edu.cn>
2024-06-26 20:16:45 +0800

    t

---
+++ src/main/java/com/resfir/database/entity/DynamicInfo.java
@@ -0,0 +1,10 @@
+package com.resfir.database.entity;
+
+import lombok.Data;
+
+@Data
+public class DynamicInfo {
+    private Integer stationCode;
+    private String stationName;
+    private int square;
+}
---
+++ src/main/java/com/resfir/database/mapper/DynamicInfoMapper.java
@@ -0,0 +1,7 @@
+package com.resfir.database.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.resfir.database.entity.DynamicInfo;
+
+public interface DynamicInfoMapper extends BaseMapper<DynamicInfo> {
+}
