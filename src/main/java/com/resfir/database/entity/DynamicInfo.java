2b674efd74fcb8e109d775ba45cdd53a507925f5
Resfir <rs3374@hotmail.com>
2024-06-26 23:37:06 +0800

    bugfix

--- src/main/java/com/resfir/database/entity/DynamicInfo.java
+++ src/main/java/com/resfir/database/entity/DynamicInfo.java
@@ -1,9 +1,12 @@
 package com.resfir.database.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 @Data
 public class DynamicInfo {
+    @TableId(type = IdType.INPUT)
     private Integer stationCode;
     private String stationName;
     private int square;
